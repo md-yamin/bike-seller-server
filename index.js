70e0eb33c2a479a86876555ae2d141b54fe04ba6
Focus4Study <f4studies@gmail.com>
2024-08-17 15:03:36 +0600

    add:vercel.jsom

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 //middleware
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://gear-up-e1f46.web.app'],
     credentials: true
 }))
 app.use(express.json())
